967d873ca2b780f7c6e579be4d1b47c80c99580c
blandashes <dawn1next@gmail.com>
2024-07-20 21:20:43 +0100

    tried to fix POST to see if that changes anything

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask, request, jsonify
 import requests
 import logging
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
+
+BULKSMS_API_URL = "https://portal.nigeriabulksms.com/api/"
+USERNAME = "tcwsecretariat@gmail.com"
+PASSWORD = "UFyd6@UxytPXi8"
+SENDER_ID = "welcome"
+
+def send_sms(parent_phone, message):
+    payload = {
+        "username": USERNAME,
+        "password": PASSWORD,
+        "message": message,
+        "sender": SENDER_ID,
+        "mobiles": parent_phone
+    }
+    response = requests.get(BULKSMS_API_URL, params=payload)
+    return response.json()
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -24,8 +41,13 @@ def handle_request():
     if request.method == 'POST':
         data = request.json
         
+        checkin_time = (datetime.now() + timedelta(hours=1)).strftime("%d-%b-%Y %H:%M:%S")
         # Log the incoming request data
         app.logger.info("Received data: %s", data)
+        if checkin_time:
+            message = f"Hello, They checked in to Global STAR 2024 at {checkin_time}."
+            response = send_sms("08029002325", message)
+            return jsonify(response)
         
         # Extract the relevant information from the webhook payload
         api_url = data.get('api_url')
