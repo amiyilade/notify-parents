305ba49d855dfe68fcd112de334dee9d73fd0a2f
blandashes <dawn1next@gmail.com>
2024-07-20 17:45:03 +0100

    added logging, removed param to check error

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 import requests
 from datetime import datetime, timedelta
+import logging
 
 app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
 BULKSMS_API_URL = "https://portal.nigeriabulksms.com/api/"
 USERNAME = "tcwsecretariat@gmail.com"
@@ -23,19 +27,14 @@ def send_sms(parent_phone, message):
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.json
-    parent_name = data.get('dynamic__116192000000047924')
-    parent_phone = data.get('dynamic__116192000000067523')
-    attendee_name = data.get('dynamic__116192000000013014')
-    order_id = data.get('orderId')
+    
+    # Log the incoming request data
+    app.logger.info("Received data: %s", data)
+    
     checkin_time = (datetime.now() + timedelta(hours=1)).strftime("%d-%b-%Y %H:%M:%S")
-
-    if parent_name and parent_phone and attendee_name and order_id:
-        message = f"Hello, {parent_name}. You were listed as the parent/guardian of {attendee_name} with ticket order ID {order_id}. They checked in to Global STAR 2024 at {checkin_time}."
-        response = send_sms(parent_phone, message)
-        return jsonify(response)
-    else:
-        return jsonify({"error": "Missing data"}), 400
+    message = f"They checked in to Global STAR 2024 at {checkin_time}."
+    response = send_sms("2348029002325", message)
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
